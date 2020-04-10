6e857b1321c0a6c5636aed58ee915ca74158c479
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-10 23:59:53 +0530

    detail screen and add  single scroll view more

--- lib/pages/character_detail_screen.dart
+++ lib/pages/character_detail_screen.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:despicable_me/models/character.dart';
+import 'package:despicable_me/styleguide.dart';
 import 'package:flutter/material.dart';
 
 class CharacterDetailScreen extends StatefulWidget {
@@ -15,6 +16,7 @@ class CharacterDetailScreen extends StatefulWidget {
 class _CharacterDetailScreenState extends State<CharacterDetailScreen> {
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
@@ -28,15 +30,39 @@ class _CharacterDetailScreenState extends State<CharacterDetailScreen> {
               )
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[ 
-              SizedBox(height: 40),              
-              Padding(
-                padding: const EdgeInsets.only(top:8.0, left: 16),
-                child: Icon(Icons.close , size: 40, color: Colors.white.withOpacity(0.9),),
-              ),
-            ],
+          SingleChildScrollView(            
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[ 
+                SizedBox(height: 40),              
+                Padding(
+                  padding: const EdgeInsets.only(top:8.0, left: 16),
+                  child: IconButton(
+                    iconSize: 40,
+                    icon:Icon(Icons.close), 
+                    color: Colors.white.withOpacity(0.9),
+                    onPressed: (){
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Image.asset(
+                    widget.character.imagePath, 
+                    height:screenHeight*0.45 ,
+                  )
+                ),
+                Text(
+                  widget.character.name,
+                  style: AppTheme.heading,
+                ),
+                Text(
+                  widget.character.description,
+                  style: AppTheme.subHeading,
+                ),
+              ],
+            ),
           ),
         ],
       ),
